8ffe10c328534e640848ed25517451fa7c3a2add
LuanArves <luanspsoares@outlook.com>
2024-09-04 00:54:55 -0300

    Refactor: Rename index.tsx and index.scss to Navbar.tsx and Navbar.scss

--- src/assets/components/navbar/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.scss'
-
-export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <h1 className="page-tittle">Filmes</h1>
-        </nav>
-    )
-}
